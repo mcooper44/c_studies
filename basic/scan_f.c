eca9495db32b07c7e3b3d782c8df3d77bb316b47
Matt Cooper <m.cooper44@gmail.com>
2024-11-24 22:10:48 -0500

    example of scanf usage

---
+++ basic/scan_f.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+	{
+	int num1, num2, sum;
+
+	printf("Enter two numbers...  ");
+	scanf("%d %d", &num1, &num2);
+	sum = num1 + num2;
+	printf("Sum = %d\n", sum);
+	return 0;
+	}
